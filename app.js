adcab8c95c332f96e1a21c41adf7c3313f20feb3
anazsama <anazsama@outlook.jp>
2020-04-17 07:44:55 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const length = 42;
+for(let i=0; i<=length; i++){
+    console.log('フィボナッチ数列でn=' + i +'の場合の結果：' + fib(i));
+}
+
+function fib(n){
+    if(n === 0 || n === 1){
+        return n;
+    }else if(n >= 2){
+        return fib(n-1) + fib(n-2);
+    }else{
+        return null;
+    }
+
+}
