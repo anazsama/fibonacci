8f3bd92174b7c5a66231ec1e8271d9191a288e00
anazsama <anazsama@outlook.jp>
2020-04-18 06:17:21 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const length = 42;
+const memo = new Map;
+memo.set(0,0);
+console.log('連想配列' + 0 +'の中身：' + memo.get(0));
+memo.set(1,1);
+console.log('連想配列' + 1 +'の中身：' + memo.get(1));
+
+const length = 40;
 for(let i=0; i<=length; i++){
     console.log('フィボナッチ数列でn=' + i +'の場合の結果：' + fib(i));
 }
@@ -9,9 +15,11 @@ function fib(n){
     if(n === 0 || n === 1){
         return n;
     }else if(n >= 2){
-        return fib(n-1) + fib(n-2);
+        let result = memo.get(n-1) + memo.get(n-2);
+        memo.set(n,result);
+        console.log('連想配列' + n +'の中身：' + memo.get(n));
+        return result;
     }else{
         return null;
     }
-
 }
